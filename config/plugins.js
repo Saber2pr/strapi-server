c40b9be1e070549f58f78a170c237dc7f6670e09
muhai <muhai.li@tuya.com>
2024-03-02 11:30:48 +0800

    chore: update

--- config/plugins.js
+++ config/plugins.js
@@ -1,19 +1,28 @@
-module.exports = ({ env }) => ({
-  email: {
-    config: {
-      provider: "nodemailer",
-      providerOptions: {
-        host: env("SMTP_HOST", "smtp.example.com"),
-        port: env("SMTP_PORT", 587),
-        auth: {
-          user: env("SMTP_USERNAME"),
-          pass: env("SMTP_PASSWORD"),
+module.exports = ({ env: _env }) => {
+  function env(key, defaultValue) {
+    return _env(key, process.env[key] || defaultValue);
+  }
+  Object.assign(env, _env);
+  env.int = _env.int;
+  env.bool = _env.bool;
+
+  return {
+    email: {
+      config: {
+        provider: "nodemailer",
+        providerOptions: {
+          host: env("SMTP_HOST", "smtp.example.com"),
+          port: env("SMTP_PORT", 587),
+          auth: {
+            user: env("SMTP_USERNAME"),
+            pass: env("SMTP_PASSWORD"),
+          },
+        },
+        settings: {
+          defaultFrom: env("SMTP_DEFAULT_FROM"),
+          defaultReplyTo: env("SMTP_DEFAULT_REPLY"),
         },
       },
-      settings: {
-        defaultFrom: env("SMTP_DEFAULT_FROM"),
-        defaultReplyTo: env("SMTP_DEFAULT_REPLY"),
-      },
     },
-  },
-});
+  };
+};
--- config/server.js
+++ config/server.js
@@ -1,10 +1,20 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
-  },
-});
+module.exports = ({ env: _env }) => {
+  function env(key, defaultValue) {
+    return _env(key, process.env[key] || defaultValue);
+  }
+  Object.assign(env, _env);
+  env.int = _env.int;
+  env.bool = _env.bool;
+  env.array = _env.array;
+
+  return {
+    host: env("HOST", "0.0.0.0"),
+    port: env.int("PORT", 1337),
+    app: {
+      keys: env.array("APP_KEYS"),
+    },
+    webhooks: {
+      populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
+    },
+  };
+};
