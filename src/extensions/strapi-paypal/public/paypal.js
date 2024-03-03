5ed1e8fccb69ca9d47aab6e0304292efea023f8b
muhai <muhai.li@tuya.com>
2024-03-03 13:46:06 +0800

    chore: update

--- src/extensions/strapi-paypal/public/paypal.js
+++ src/extensions/strapi-paypal/public/paypal.js
@@ -1,13 +1,13 @@
 // @ts-nocheck
 /* eslint-disable no-undef */
-"use strict";
+'use strict';
 
 window.onload = () => {
   // for product Checkout
-  const ppProduct = document.querySelectorAll(".PP_ProductCheckout");
+  const ppProduct = document.querySelectorAll('.PP_ProductCheckout');
   if (ppProduct) {
-    ppProduct.forEach((product) => {
-      product.addEventListener("click", function handleClick(event) {
+    ppProduct.forEach(product => {
+      product.addEventListener('click', function handleClick(event) {
         PP_ProductCheckout(event.target.dataset.id, event.target.dataset.url);
       });
     });
@@ -17,24 +17,21 @@ window.onload = () => {
 // product Checkout logic
 
 function PP_ProductCheckout(productId, baseUrl) {
-  localStorage.setItem("strapiPaypalUrl", baseUrl);
-  const getProductApi =
-    baseUrl + "/strapi-paypal/getPaypalCheckout/" + productId;
+  localStorage.setItem('strapiPaypalUrl', baseUrl);
+  const getProductApi = baseUrl + '/strapi-paypal/getPaypalCheckout/' + productId;
   // const checkoutSessionUrl = baseUrl + "/strapi-paypal/createCheckoutSession/";
 
   fetch(getProductApi, {
-    method: "get",
-    mode: "cors",
+    method: 'get',
+    mode: 'cors',
     headers: new Headers({
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     }),
   })
-    .then((response) => response.json())
-    .then((response) => {
+    .then(response => response.json())
+    .then(response => {
       if (response.id) {
-        const filter = response.links.filter(
-          (links) => links.rel === "approve"
-        );
+        const filter = response.links.filter(links => links.rel === 'approve');
         window.location.replace(filter[0].href);
       }
     });
